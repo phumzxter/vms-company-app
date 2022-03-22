8c34a8ba5dcff29a7804ceebaaa16a7c39d0a0f8
Phumin Abdul Hameed <59028111+phumzxter@users.noreply.github.com>
2022-03-23 02:10:24 +0530

    c

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, "public", "react-quotation"))); 
 
-app.get("//*", function (req, res) { 
+app.get("/react-quotation/*", function (req, res) { 
 
   res.sendFile(path.join(__dirname, "public", "react-quotation", "index.html")); 
 
